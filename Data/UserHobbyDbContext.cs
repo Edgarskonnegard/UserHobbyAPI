66b0e82f3090111fca9b797f9fc10d451753c04b
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2025-04-23 18:17:16 +0200

    Added Seed-Data

--- Data/UserHobbyDbContext.cs
+++ Data/UserHobbyDbContext.cs
@@ -16,9 +16,10 @@ public class UserHobbyDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
         modelBuilder.Entity<UserHobby>()
             .HasKey(uh => uh.Id);
-            
+
         modelBuilder.Entity<UserHobby>()
             .HasOne( uh => uh.User )
             .WithMany( u => u.UserHobbies )
@@ -28,6 +29,23 @@ public class UserHobbyDbContext : DbContext
             .HasOne( uh => uh.Hobby)
             .WithMany( h => h.UserHobbies )
             .HasForeignKey( uh => uh.HobbyId);
+
+        //Seed-Data
+        modelBuilder.Entity<User>().HasData(
+            new User {Id = 1, FirstName = "Anna", LastName = "Andersson", BirthYear = 1990 },
+            new User {Id = 2, FirstName="Björn", LastName="Berg", BirthYear=1985}
+        );
+
+        modelBuilder.Entity<Hobby>().HasData(
+            new Hobby {Id=1, Name="Löpning", Description="Springa i skogen"},
+            new Hobby {Id=2, Name="Gitarr", Description="Spela akustisk gitarr"}
+        );
+
+        modelBuilder.Entity<UserHobby>().HasData(
+            new UserHobby {Id=1, UserId = 1, HobbyId = 1, Url="https://google.com"},
+            new UserHobby {Id=2, UserId = 1, HobbyId = 2, Url="https://facebook.com"},
+            new UserHobby {Id=3, UserId = 2, HobbyId=2, Url="https://github.com"}
+        );
 
     }
 }
